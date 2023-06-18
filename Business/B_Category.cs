59234771441c5332599fbaa04a9eb23725515a0a
Miguel Gonzalez <mig_24@hotmail.com>
2023-06-18 17:14:24 -0500

    Proyecto final de blazor en curso platzi blazor 3.1

---
+++ Business/B_Category.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities;
+using DataAccess;
+using System.Linq;
+
+namespace Business
+{
+    public class B_Category
+    {
+        public static List<CategoryEntity> CategoryList()
+        {
+            using (var db = new InventaryContext())
+            {
+                return db.Categories.ToList();
+            }
+        }
+
+        public static CategoryEntity CategoryById(string categoryId)
+        {
+            using (var db = new InventaryContext())
+            {
+                return db.Categories
+                     .ToList()
+                    .LastOrDefault(c => c.CategoryId == categoryId);
+            }
+        }
+
+        public static void CreateCategory(CategoryEntity oCategory)
+        {
+            using (var db = new InventaryContext()) 
+            {
+                db.Categories.Add(oCategory);
+                db.SaveChanges();
+            }
+        }
+
+        public static void UpdateCategory(CategoryEntity oCategory)
+        {
+            using(var db = new InventaryContext())
+            {
+                db.Categories.Update(oCategory);
+                db.SaveChanges();
+            }
+        }
+    }
+}
